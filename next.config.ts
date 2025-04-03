6496b427e59c5ff62d444a814d25048b03906b50
devidardhruvil-codes <dsilento01@gmail.com>
2025-04-03 22:08:40 +0530

    mobile navigation implemented

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "img.freepik.com",
       },
+      {
+        protocol: "https",
+        hostname: "cdn1.iconfinder.com",
+      },
     ],
   },
 };
